666ed9ab7aae4036e03f52364e9aff7592bf889e
Your Name <your-email@example.com>
2026-05-08 19:06:48 -0500

    s

--- users/work.nix
+++ users/work.nix
@@ -443,12 +443,41 @@
     # ── Screenshot ──────────────────────────────────────────────────────────────
     
   ];
-services.flameshot = {
+
+sservices.flameshot = {
   enable = true;
   settings = {
     General = {
-      # disabledGrimWarning = true;
-      showDesktopNotification = false; # Speeds it up on low-end CPUs
+        
+      # More settings may be found on the Flameshot Github
+
+      # Save Path
+      savePath = "/home/user/Screenshots";
+      # Tray
+      disabledTrayIcon = true;
+      # Greeting message   
+      showStartupLaunchMessage = false;
+      # Default file extension for screenshots (.png by default)
+      saveAsFileExtension = ".png";
+      # Desktop notifications
+      showDesktopNotification = true;
+      # Notification for cancelled screenshot
+      showAbortNotification = false;
+      # Whether to show the info panel in the center in GUI mode
+      showHelp = true;
+      # Whether to show the left side button in GUI mode
+      showSidePanelButton = true;
+
+
+      # Color Customization
+      uiColor = "#740096";
+      contrastUiColor = "#270032";
+      drawColor = "#ff0000";
+
+      # For Wayland (Install Grim seperately)
+      useGrimAdapter = true;
+      # Stops warnings for using Grim
+      disabledGrimWarning = true;
     };
   };
 };

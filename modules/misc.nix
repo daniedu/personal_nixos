bf04179205013560e6e65ecde0f1afaac5b7780a
Your Name <your-email@example.com>
2026-05-16 15:14:26 -0500

    enable experimental nix features (nix-command, flakes)

--- modules/misc.nix
+++ modules/misc.nix
@@ -1,5 +1,6 @@
 { ... }: {
   nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
     extra-substituters = [ "https://vicinae.cachix.org" ];
     extra-trusted-public-keys = [ "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc=" ];
   };

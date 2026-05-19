a4ab64f9fd6ce36e43f9aed0cab8bbb981bd267f
Your Name <your-email@example.com>
2026-05-19 15:22:19 -0500

    fixing portal

--- modules/boot.nix
+++ modules/boot.nix
@@ -6,4 +6,11 @@
   };
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  systemd.user.services.xdg-desktop-portal = {
+  Service = {
+    Environment = [
+      "NIX_XDG_DESKTOP_PORTAL_DIR=/run/current-system/sw/share/xdg-desktop-portal/portals"
+    ];
+  };
+}; 
 }

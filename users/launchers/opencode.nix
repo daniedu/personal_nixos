0bab47912e5d7363a9e3f46360ed359d215b9652
Your Name <your-email@example.com>
2026-05-20 15:02:07 -0500

    fixing opencode

--- users/launchers/opencode.nix
+++ users/launchers/opencode.nix
@@ -3,7 +3,7 @@ let
   opencode-version = "1.15.5";
   opencode-src = pkgs.fetchurl {
     url = "https://github.com/anomalyco/opencode/releases/download/v${opencode-version}/opencode-linux-x64.tar.gz";
-    sha256 = "v2912gibIgc7zyN1TMO+NR9xM2MWTlvc0+SVAcgRscU";
+    sha256 = "v2912gibIgc7zyN1TMO+NR9xM2MWTlvc0+SVAcgRscU=";
   };
   opencode-bin = pkgs.runCommandLocal "opencode-${opencode-version}" { } ''
     mkdir -p $out/bin

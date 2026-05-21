9dbb3c2a2a641fdb05b72bc857a087f9bc9ca90d
Your Name <your-email@example.com>
2026-05-21 11:01:57 -0500

    s

--- modules/tablet.nix
+++ modules/tablet.nix
@@ -5,4 +5,7 @@
   # Required by OpenTabletDriver
   hardware.uinput.enable = true;
   boot.kernelModules = [ "uinput" ];
+  services.udev.extraRules = ''
+    KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess"
+  '';
 }

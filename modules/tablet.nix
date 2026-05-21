5b6868b85debc8d87b240d5ff85ae56fa33576c1
Your Name <your-email@example.com>
2026-05-21 11:26:44 -0500

    fix open tablet driver by blacklisting wacom and hid uclogic kernel modules

--- modules/tablet.nix
+++ modules/tablet.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }: {
- # Enable OpenTabletDriver
   hardware.opentabletdriver.enable = true;
-
-  # Required by OpenTabletDriver
   hardware.uinput.enable = true;
+
   boot.kernelModules = [ "uinput" ];
+  boot.blacklistedKernelModules = [ "wacom" "hid_uclogic" ];
+
   services.udev.extraRules = ''
     KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess"
   '';

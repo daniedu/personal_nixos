44b777013e6d62800f290c5cee9db4f43be5c7fe
Your Name <your-email@example.com>
2026-05-21 10:20:01 -0500

    s

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -5,6 +5,12 @@
     dedicatedServer.openFirewall = true;
     gamescopeSession.enable    = true;
     extraCompatPackages = with pkgs; [ proton-ge-bin ];
+
+    gamescopeSession.args = [
+        "-W 3460"
+        "-H 2160"
+        "-f" # Force Fullscreen immediately
+      ];
   };
 
   programs.gamescope = {
@@ -13,5 +19,20 @@
   };
 
   programs.gamemode.enable = true;
+  
   hardware.xone.enable = true;
+  
+  services.displayManager.sessionPackages = [
+    (pkgs.writeTextFile {
+      name = "steam-direct-session";
+      destination = "/share/xsessions/steam-direct.desktop"; # Using X11 session for older Intel drivers
+      text = ''
+        [Desktop Entry]
+        Name=Steam Big Picture Mode
+        Comment=Boot directly into Steam without a heavy desktop environment
+        Exec=unclutter -idle 1 & steam -bigpicture
+        Type=Application
+      '';
+    })
+  ];
 }

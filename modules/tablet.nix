c860539402e34de55f589cdb0d991ff043ad39ec
Your Name <your-email@example.com>
2026-05-21 10:54:58 -0500

    s

--- modules/tablet.nix
+++ modules/tablet.nix
@@ -1,3 +1,8 @@
 { pkgs, ... }: {
+ # Enable OpenTabletDriver
   hardware.opentabletdriver.enable = true;
+
+  # Required by OpenTabletDriver
+  hardware.uinput.enable = true;
+  boot.kernelModules = [ "uinput" ];
 }

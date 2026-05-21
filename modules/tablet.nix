7878618db15eac01f5ed231b2818c31cbec2f6d0
Your Name <your-email@example.com>
2026-05-21 11:38:57 -0500

    s

--- modules/tablet.nix
+++ modules/tablet.nix
@@ -2,10 +2,10 @@
   hardware.opentabletdriver.enable = true;
   hardware.uinput.enable = true;
 
-  boot.kernelModules = [ "uinput" ];
-  boot.blacklistedKernelModules = [ "wacom" "hid_uclogic" ];
+  # boot.kernelModules = [ "uinput" ];
+  # boot.blacklistedKernelModules = [ "wacom" "hid_uclogic" ];
 
-  services.udev.extraRules = ''
-    KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess"
-  '';
+  # services.udev.extraRules = ''
+  #   KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess"
+  # '';
 }

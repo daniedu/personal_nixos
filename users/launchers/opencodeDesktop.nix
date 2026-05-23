8fcb2c989739d69b407edb50099842262af7871a
Your Name <your-email@example.com>
2026-05-23 14:39:05 -0500

    fixing opencode desktop

--- users/launchers/opencodeDesktop.nix
+++ users/launchers/opencodeDesktop.nix
@@ -9,30 +9,18 @@ let
     sha256 = "sha256-CmBtCgCd5tGDF2BTEdigNAmyTnC3KZp8pypartsaH68=";
   };
 
-  contents = pkgs.appimageTools.extractType2 {
+  app = pkgs.appimageTools.wrapType2 {
     inherit pname version src;
-  };
-
-  opencode-desktop = pkgs.appimageTools.wrapType2 {
-    inherit pname version src;
-
-    extraInstallCommands = ''
-  install -Dm444 ${pkgs.writeText "opencode.desktop" ''
-    [Desktop Entry]
-    Name=Opencode Desktop
-    Exec=opencode-desktop %U
-    Icon=opencode-desktop
-    Type=Application
-    Categories=Development;
-  ''} $out/share/applications/opencode.desktop
-
-  install -Dm444 ${contents}/opencode.png \
-    $out/share/icons/hicolor/512x512/apps/opencode-desktop.png
-'';
   };
 
 in {
-  home.packages = [
-    opencode-desktop
-  ];
+  home.packages = [ app ];
+
+  xdg.desktopEntries.opencode-desktop = {
+    name = "Opencode Desktop";
+    exec = "opencode-desktop %U";
+    icon = "applications-development";
+    categories = [ "Development" ];
+    terminal = false;
+  };
 }

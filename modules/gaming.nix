993c67e752d5ac28e1fe08c6d6e3b3dd93b765d6
Your Name <your-email@example.com>
2026-05-25 19:15:40 -0500

    reverting gaming dependecies, pc not good for x11

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -3,39 +3,17 @@
     enable                     = true;
     remotePlay.openFirewall    = true;
     dedicatedServer.openFirewall = true;
-    gamescopeSession.enable    = false;
+    gamescopeSession.enable    = true;
     extraCompatPackages = with pkgs; [ proton-ge-bin ];
 
   };
 
-  # programs.gamescope = {
-  #   enable      = false;
-  #   capSysNice  = true;
-  # };
+  programs.gamescope = {
+    enable      = true;
+    capSysNice  = true;
+  };
 
-  # programs.gamemode.enable = true;
+  programs.gamemode.enable = true;
   
-  # hardware.xone.enable = true;
-  
-  services.displayManager.sessionPackages = let
-    # 1. Create the actual desktop definition file
-    steamDesktopFile = pkgs.writeTextFile {
-      name = "steam-direct-x11-file";
-      destination = "/share/xsessions/steam-direct.desktop";
-      text = ''
-        [Desktop Entry]
-        Name=Steam Big Picture
-        Comment=Launch Steam directly using native hardware OpenGL acceleration
-        Exec=steam -bigpicture
-        Type=Application
-      '';
-    };
-  in [
-    # 2. Wrap it with the metadata NixOS requires to pass validation
-    (pkgs.symlinkJoin {
-      name = "steam-direct-x11";
-      paths = [ steamDesktopFile ];
-      passthru.providedSessions = [ "steam-direct" ];
-    })
-  ];
+  hardware.xone.enable = true;
 }

30ec06df4991acd498f3c47fe71e658ec8cab89f
Your Name <your-email@example.com>
2026-07-28 16:17:49 -0500

    fix: xdg-portal.nix syntax error - extra closing braces and bad indentation
    The config block was mis-indented and had 2 extra closing braces
    that caused a nix build syntax error at the xdg.portal attribute set.

--- modules/xdg-portal.nix
+++ modules/xdg-portal.nix
@@ -7,18 +7,17 @@
       xdg-desktop-portal-wlr
       xdg-desktop-portal-gtk
     ];
-config = {
-    common = {
-      default = [ "gtk" ];
+    config = {
+      common = {
+        default = [ "gtk" ];
+      };
+      hyprland = {
+        default = [ "hyprland" "gtk" ];
+      };
+      mango = {
+        default = lib.mkForce [ "wlr" "gtk" ];
+      };
     };
-    hyprland = {
-      default = [ "hyprland" "gtk" ];
-    };
-    mango = {
-      default = lib.mkForce [ "wlr" "gtk" ];
-    };
-  };
-  };
   };
   services.dbus.enable = true;
   services.gvfs.enable = true;

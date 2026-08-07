5ebee3d8036335208665d338a1688e8ff2972de3
Your Name <your-email@example.com>
2026-08-07 08:53:30 -0500

    adding new wallpapers and updating opacity

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }: {
   stylix = {
-    enable     = true;
+    enable = true;
     autoEnable = true;
     targets = {
       gnome.enable = false;
@@ -11,25 +11,25 @@
     fonts = {
       monospace = {
         package = pkgs.nerd-fonts.jetbrains-mono;
-        name    = "JetBrainsMono Nerd Font Mono";
+        name = "JetBrainsMono Nerd Font Mono";
       };
       sansSerif = {
         package = pkgs.noto-fonts;
-        name    = "Noto Sans";
+        name = "Noto Sans";
       };
       serif = {
         package = pkgs.noto-fonts;
-        name    = "Noto Serif";
+        name = "Noto Serif";
       };
       emoji = {
         package = pkgs.noto-fonts-color-emoji;
-        name    = "Noto Color Emoji";
+        name = "Noto Color Emoji";
       };
       sizes = {
         applications = 11;
-        terminal     = 11;
-        desktop      = 11;
-        popups       = 11;
+        terminal = 11;
+        desktop = 11;
+        popups = 11;
       };
     };
 
@@ -39,15 +39,15 @@
 
     cursor = {
       package = pkgs.bibata-cursors;
-      name    = "Bibata-Modern-Classic";
-      size    = 24;
+      name = "Bibata-Modern-Classic";
+      size = 24;
     };
 
     opacity = {
-      terminal     = 0.90;
+      terminal = 0.97;
       applications = 1.0;
-      popups       = 0.90;
-      desktop      = 1.0;
+      popups = 0.90;
+      desktop = 1.0;
     };
   };
 }

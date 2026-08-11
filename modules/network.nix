84f41391101b91a8e2b1a574c54ebe089cb04640
Your Name <your-email@example.com>
2026-08-11 15:12:44 -0500

    adding ports

--- modules/network.nix
+++ modules/network.nix
@@ -1,6 +1,13 @@
 { ... }: {
-  networking.hostName              = "dan";
+  networking.hostName = "dan";
   networking.networkmanager.enable = true;
-  networking.firewall.allowedTCPPorts = [ 21115 21116 21117 ];
+  networking.firewall.allowedTCPPorts = [
+    21114
+    21115
+    21116
+    21117
+    21118
+    21119
+  ];
   networking.firewall.allowedUDPPorts = [ 21116 ];
 }

67b47d6b7cdf127e0810cc45f022118a5616e030
Your Name <your-email@example.com>
2026-08-11 15:04:38 -0500

    adding rust desk ports

--- modules/network.nix
+++ modules/network.nix
@@ -1,4 +1,6 @@
 { ... }: {
   networking.hostName              = "dan";
   networking.networkmanager.enable = true;
+  networking.firewall.allowedTCPPorts = [ 21115 21116 21117 ];
+  networking.firewall.allowedUDPPorts = [ 21116 ];
 }

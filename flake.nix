871267eca65855b04f45dff0cfc3772132ddf509
Your Name <your-email@example.com>
2026-08-13 15:00:42 -0500

    adding zen browser

--- flake.nix
+++ flake.nix
@@ -20,6 +20,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     mangowm = {
       url = "github:mangowm/mango";
       inputs.nixpkgs.follows = "nixpkgs";
--- modules/packages.nix
+++ modules/packages.nix
@@ -32,6 +32,7 @@
 
     # === Extras ===
     inputs.helium.packages.${pkgs.stdenv.hostPlatform.system}.default
+    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
     localsend
   ];
 
